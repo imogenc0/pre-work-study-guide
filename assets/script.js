6646646de1d29149e09c6ed12fcd9d036db2fec1
imogenc0 <imogencoughlin@gmail.com>
2023-09-26 16:16:35 +0100

    coded Javascript part 2 - functions and edited index.html to add notes

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
-var topic = ['HTML', 'CSS', 'Git', 'Javascript'];
-for (var x = 0; x < topic.length; x++) {
-    console.log(topic[x])
-}
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-/*
-if (topic === 'HTML') {
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+}
+function selectTopic() {
+if (topics[0] === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (topics[0] === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (topics[0] === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'Javascript') {
+} else if (topics[0] === 'Javascript') {
     console.log("Let's study Javascript!");
 } else {
     console.log('Please try again!');
-}*/
+}
+}
+console.log('Here are the topics we learned through Prework');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
